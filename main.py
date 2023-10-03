8276e5e489c1ad56b857eceaef1fd79e8a8e0367
Gonza <gonza56d@gmail.com>
2023-10-03 14:27:51 -0300

    JAJA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.core.engine import run_game
 
 if __name__ == '__main__':
-    results = run_game(runs=1000)
-    print(f'{results.changed_corrects} victory games with player changing the door.')
-    print(f'{results.not_changed_corrects} victory games with player NOT changing the door.')
+    runs = 1000
+    results = run_game(runs)
+    print(f'{results.changed_corrects} out of {runs} victory games with player changing the door.')
+    print(f'{results.not_changed_corrects} out of {runs} victory games with player NOT changing the door.')
